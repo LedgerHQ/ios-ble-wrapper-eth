9384130aebc072f003bd391242082a7a8fae06ce
Dante P <dantejpuglisi@gmail.com>
2022-07-18 19:00:32 -0300

    Added async methods and some UI

--- EthereumWrapperDemo/EthereumWrapperDemo/ViewController.swift
+++ EthereumWrapperDemo/EthereumWrapperDemo/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var connectionLabel: UILabel!
     @IBOutlet weak var getAppConfigurationButton: UIButton!
     @IBOutlet weak var getAddressButton: UIButton!
+    @IBOutlet weak var openAppButton: UIButton!
+    @IBOutlet weak var closeAppButton: UIButton!
     
     let DERIVATION_PATH_ETH = "44'/60'/0'/0/0"
     let RAW_TX_HEX_TEST = "02f90115010384773594008518abb54a008302ceed94def171fe48cf0115b1d80b88dc8eab59176fee5787084701707a11e7b8e4b2f1e6db000000000000000000000000eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee00000000000000000000000000000000000000000000000000084701707a11e700000000000000000000000000000000000000000000000029a2241af62c0000000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc200000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000001000000000000000000004de4bf58a4077c71b5699bd19287eb76beaba5361bbfc0"
@@ -29,16 +31,22 @@ class ViewController: UIViewController {
         func connect() {
             self.getAppConfigurationButton.isEnabled = false
             self.getAddressButton.isEnabled = false
+            self.openAppButton.isEnabled = false
+            self.closeAppButton.isEnabled = false
             
-            BleTransport.shared.create {
+            BleTransport.shared.create(timeout: .seconds(10)) {
+                self.eth.bleConnected = false
                 print("Device disconnected")
                 connect()
                 self.connectionLabel.text = "Reconnecting..."
             } success: { connectedPeripheral in
+                self.eth.bleConnected = true
                 self.connectionLabel.text = "Connected to \(connectedPeripheral.name)"
                 print("Connected to peripheral with name: \(connectedPeripheral.name)")
                 self.getAppConfigurationButton.isEnabled = true
                 self.getAddressButton.isEnabled = true
+                self.openAppButton.isEnabled = true
+                self.closeAppButton.isEnabled = true
             } failure: { error in
                 if let error = error {
                     print(error.description())
@@ -67,6 +75,29 @@ class ViewController: UIViewController {
             self.waitingForResponseLabel.text = "\(response)"
         } failure: { error in
             self.waitingForResponseLabel.text = "ERROR: \(error)"
+        }
+    }
+    
+    @IBAction func openAppButtonTapped(_ sender: Any) {
+        Task() {
+            print("Will try opening Ethereum")
+            do {
+                try await eth.openAppIfNeeded()
+                print("Opened Ethereum!")
+            } catch {
+                print("\((error as? BleTransportError)?.description() ?? "Failed with no error")")
+            }
+        }
+    }
+    
+    @IBAction func closeAppButtonTapped(_ sender: Any) {
+        Task() {
+            do {
+                try await eth.closeApp()
+                print("Closed app!")
+            } catch {
+                print("\((error as? BleTransportError)?.description() ?? "Failed with no error")")
+            }
         }
     }
 }
--- Sources/EthWrapper/EthWrapper.swift
+++ Sources/EthWrapper/EthWrapper.swift
@@ -7,6 +7,7 @@
 import Foundation
 import JavaScriptCore
 import BleWrapper
+import BleTransport
 import os
 
 public class EthWrapper: BleWrapper {
@@ -65,6 +66,74 @@ public class EthWrapper: BleWrapper {
         ethInstance = ethModule.construct(withArguments: [transportInstance])
     }
     
+    // MARK: - Async methods
+    public func openApp() async throws {
+        return try await super.openApp("Ethereum")
+    }
+    
+    public func openAppIfNeeded() async throws {
+        return try await super.openAppIfNeeded("Ethereum")
+    }
+    
+    public func getAppConfiguration() async throws -> [AnyHashable: Any] {
+        return try await withCheckedThrowingContinuation { continuation in
+            getAppConfiguration { response in
+                continuation.resume(returning: response)
+            } failure: { error in
+                continuation.resume(throwing: BleTransportError.lowerLevelError(description: error))
+            }
+        }
+    }
+    
+    public func getAddress(path: String, boolDisplay: Bool, boolChaincode: Bool) async throws -> [AnyHashable: Any] {
+        return try await withCheckedThrowingContinuation { continuation in
+            getAddress(path: path, boolDisplay: boolDisplay, boolChaincode: boolChaincode) { response in
+                continuation.resume(returning: response)
+            } failure: { error in
+                continuation.resume(throwing: BleTransportError.lowerLevelError(description: error))
+            }
+        }
+    }
+    
+    public func signTransaction(path: String, rawTxHex: String) async throws -> [AnyHashable: Any] {
+        return try await withCheckedThrowingContinuation { continuation in
+            signTransaction(path: path, rawTxHex: rawTxHex) { response in
+                continuation.resume(returning: response)
+            } failure: { error in
+                continuation.resume(throwing: BleTransportError.lowerLevelError(description: error))
+            }
+        }
+    }
+    
+    public func signPersonalMessage(path: String, messageHex: String) async throws -> [AnyHashable: Any] {
+        return try await withCheckedThrowingContinuation { continuation in
+            signPersonalMessage(path: path, messageHex: messageHex) { response in
+                continuation.resume(returning: response)
+            } failure: { error in
+                continuation.resume(throwing: BleTransportError.lowerLevelError(description: error))
+            }
+        }
+    }
+    
+    public func signEIP712HashedMessage(path: String, domainSeparatorHex: String, hashStructMessageHex: String) async throws -> [AnyHashable: Any] {
+        return try await withCheckedThrowingContinuation { continuation in
+            signEIP712HashedMessage(path: path, domainSeparatorHex: domainSeparatorHex, hashStructMessageHex: hashStructMessageHex) { response in
+                continuation.resume(returning: response)
+            } failure: { error in
+                continuation.resume(throwing: BleTransportError.lowerLevelError(description: error))
+            }
+        }
+    }
+    
+    // MARK: - Completion methods
+    public func openApp(success: @escaping EmptyResponse, failure: @escaping ErrorResponse) {
+        super.openApp("Ethereum", success: success, failure: failure)
+    }
+    
+    public func openAppIfNeeded(completion: @escaping (Result<Void, BleTransportError>) -> Void) {
+        super.openAppIfNeeded("Ethereum", completion: completion)
+    }
+    
     public func getAppConfiguration(success: @escaping DictionaryResponse, failure: @escaping StringResponse) {
         invokeMethod(.getAppConfiguration, arguments: [], success: { resolve in
             if let dict = resolve.toDictionary() {
@@ -115,6 +184,7 @@ public class EthWrapper: BleWrapper {
         }, failure: failure)
     }
     
+    // MARK: - Private methods
     fileprivate func invokeMethod(_ method: Method, arguments: [Any], success: @escaping JSValueResponse, failure: @escaping StringResponse) {
         guard let ethInstance = ethInstance else { failure("Instance not initialized"); return }
         ethInstance.invokeMethodAsync(method.rawValue, withArguments: arguments, completionHandler: { resolve, reject in
